7c063e363bdb015fd21da94c751602b8ac521d17
Mr Banana <willy28@batman.com>
2024-04-25 16:44:18 +0100

    desktop done

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 //sigh*
 //getting the modal working
 //unfortunately the modal didn't work...so sad
+
+//trying a preloader
+
+const loader = document.querySelector("#preloader");
+
+window.addEventListener("load", () => {
+    loader.style.display = "none";
+    loader.style.visibility = "hidden";
+    loader.style.opacity = "0";
+})
